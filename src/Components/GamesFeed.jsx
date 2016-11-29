485e083b76041922198d15f7006a4d8882e984f5
Javier Suarez <javiersuarezl@outlook.com>
2016-11-28 22:09:38 -0600

    :m: Map the state
    Adds a component for each game on the firebase database. Still needs
    lots of work to be done

---
+++ src/Components/GamesFeed.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Grid, Cell, CardText, CardActions, IconButton, Menu,
+  MenuItem, Button, Card } from 'react-mdl';
+import fb from '../firebase';
+
+export default class GamesFeed extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      juegos: []
+    };
+  }
+
+  componentWillMount() {
+    /*
+     * Here we call 'bindToState', which will update
+     * our local 'juegos' state whenever our 'games'
+     * Firebase endpoint changes.
+     */
+
+    fb.bindToState('games', {
+      context: this,
+      state: 'juegos',
+      asArray: true
+    });
+  }
+
+  render() {
+    var GameCards = this.state.juegos.map((game, index) => {
+      return (
+        <Grid key={index} component="section" className="section--center" shadow={0} noSpacing>
+          <Cell component={Card} col={12}>
+            <CardText>
+              <h4> Game #{index}</h4>
+              <p>Player 1: { game.p1 }</p>
+              <p>Player 2: { game.p2 }</p>
+            </CardText>
+            <CardActions>
+              <Button href="#">See full game</Button>
+            </CardActions>
+          </Cell>
+          <IconButton name="more_vert" id="btn1" ripple />
+          <Menu target="btn1" align="right" valign="bottom">
+            <MenuItem>Lorem</MenuItem>
+            <MenuItem disabled>Ipsum</MenuItem>
+            <MenuItem>Dolor</MenuItem>
+          </Menu>
+        </Grid>
+      );
+    });
+
+    return (
+      <div>
+        { GameCards }
+      </div>
+    );
+  }
+}
--- src/Components/MainBody.jsx
+++ src/Components/MainBody.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Content, Grid, Cell, Icon, CardText, CardActions, IconButton, Menu,
-  MenuItem, Button, Card } from 'react-mdl';
+import { Content } from 'react-mdl';
+import GamesFeed from './GamesFeed.jsx';
 import fb from '../firebase';
 
 export default class MainBody extends React.Component {
@@ -28,43 +28,7 @@ export default class MainBody extends React.Component {
   render() {
     return (
       <Content component="main" className="react-mdl-layout__tab-panel">
-        <Grid component="section" className="section--center" shadow={0} noSpacing>
-          <Cell component="header" col={3} tablet={2} phone={4} >
-            <Icon name="play_circle_filled" />
-          </Cell>
-          <Cell component={Card} col={9} tablet={6} phone={4}>
-            <CardText>
-              <h4>Features</h4>
-              <p>{ this.state.juegos.length }</p>
-            </CardText>
-            <CardActions>
-              <Button href="#">Read our features</Button>
-            </CardActions>
-          </Cell>
-          <IconButton name="more_vert" id="btn1" ripple />
-          <Menu target="btn1" align="right" valign="bottom">
-            <MenuItem>Lorem</MenuItem>
-            <MenuItem disabled>Ipsum</MenuItem>
-            <MenuItem>Dolor</MenuItem>
-          </Menu>
-        </Grid>
-        <Grid component="section" className="section--center" shadow={0} noSpacing>
-          <Cell component={Card} col={12}>
-            <CardText>
-              <h4>Technology</h4>
-              Dolore ex deserunt aute fugiat aute nulla ea sunt aliqua nisi c.
-            </CardText>
-            <CardActions>
-              <Button href="#">Read our features</Button>
-            </CardActions>
-          </Cell>
-          <IconButton name="more_vert" id="btn3" ripple />
-          <Menu target="btn3" align="right" valign="bottom">
-            <MenuItem>Lorem</MenuItem>
-            <MenuItem disabled>Ipsum</MenuItem>
-            <MenuItem>Dolor</MenuItem>
-          </Menu>
-        </Grid>
+        <GamesFeed />
       </Content>
     );
   }
