b1f43d43e87f11408b3d4984f778c3269ea39d15
Javier Suarez <javiersuarezl@outlook.com>
2016-12-03 21:20:15 -0600

    Firebase Admin SDK
    - Adds Firebase Admin on server code, which now handles basic requests
    to the game logic
    - Also added *.key.* to .gitignore to ignore the firebase admin secret
    file

--- api/Controllers/GameController.js
+++ api/Controllers/GameController.js
@@ -1,20 +1,69 @@
-// import fb from '../firebase';
+const admin = require('firebase-admin');
+const serviceAccount = require('../../stephen-pong-firebase-adminsdk.key.json');
+
+// TODO: Validate the objects before sending to firebase,
+// maybe create schema for games objetcs
 
 class GameController {
-  static getGames() {
-    return 'NYI: Get games';
+
+  constructor() {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: 'https://stephen-pong.firebaseio.com'
+    });
+    this.db = admin.database();
   }
 
-  static addGame() {
-    return 'NYI: Add new game, return new gameId';
+  getGames() {
+    const that = this;
+    return new Promise(function(resolve, reject) {
+      let games = [];
+      that.db.ref('games').once('value', function(res) {
+        res.forEach(function(obj) {
+          games.push({
+            [obj.key]: obj.val()
+          });
+        });
+        if (games.length > 0) {
+          resolve(games);
+        } else {
+          reject(Error('Network error'));
+        }
+      });
+    });
   }
 
-  static getGame(params) {
-    return 'NYI: Get game ' + params.idGame + ' info';
+  addGame() {
+    const gameId = this.db.push();
+    gameId.set({
+      createTime: new Date(),
+      tableId: 1,
+      blue: 'foo',
+      red: 'bar'
+    });
+    return gameId.key;
   }
 
-  static updateGame(params) {
-    return 'NYI: Update game' + params.idGame + ' info';
+  getGame(params) {
+    const that = this;
+    return new Promise(function(resolve, reject) {
+      that.db.ref('games/' + params.idGame).once('value').then(function(res) {
+        if (res.key) {
+          resolve({
+            [res.key]: res.val()
+          });
+        } else {
+          reject(Error('Network error'));
+        }
+      });
+    });
+  }
+
+  updateGame(params, query) {
+    query['updateTime'] = new Date();
+    const gameRef = this.db.child(params.idGame);
+    gameRef.update(query);
+    return gameRef.key;
   }
 
 }
--- api/routes.js
+++ api/routes.js
@@ -1,6 +1,8 @@
 const routes = require('express').Router();
 const GameController = require('./Controllers/GameController');
 
+const GC = new GameController();
+
 routes.use(function timeLog(req, res, next) {
   console.log('API Request ->', req.method, req.url, '@', new Date());
   next();
@@ -11,20 +13,30 @@ routes.get('/', (req, res) => {
 });
 
 routes.get('/games', (req, res) => {
-  res.send(GameController.getGames());
+  GC.getGames().then(function(response) {
+    res.status(200).json(response);
+  }, function(error) {
+    console.error('API Error ->', req.method, req.url, '@', new Date(), error);
+    res.status(503).send('Bad Request');
+  });
 });
 
 routes.post('/games', (req, res) => {
   console.log(req.params);
-  res.send(GameController.addGame());
+  res.send(GC.addGame());
 });
-//
+
 routes.get('/game/:idGame', (req, res) => {
-  res.send(GameController.getGame(req.params));
+  GC.getGame(req.params).then(function(response) {
+    res.status(200).json(response);
+  }, function(error) {
+    console.error('API Error ->', req.method, req.url, '@', new Date(), error);
+    res.status(503).send('Bad Request');
+  });
 });
 
 routes.put('/game/:idGame', function(req, res) {
-  res.send(GameController.updateGame(req.params));
+  res.send(GC.updateGame(req.params, req.query));
 });
 
 module.exports = routes;
